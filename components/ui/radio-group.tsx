2ddbd250d3e91538c45179c3e664b255cb327eb1
prathyushk1 <prathyushk89@gmail.com>
2025-11-23 11:06:10 +0530

    Initial commit

--- components/ui/radio-group.tsx
+++ components/ui/radio-group.tsx
@@ -3,28 +3,49 @@
 import * as React from "react"
 import { cn } from "@/lib/utils"
 
-const RadioGroup = React.forwardRef<
-  HTMLDivElement,
-  React.HTMLAttributes<HTMLDivElement>
->(({ className, ...props }, ref) => {
-  return (
-    <div
-      ref={ref}
-      className={cn("grid gap-2", className)}
-      {...props}
-    />
-  )
-})
+interface RadioGroupProps extends Omit<React.HTMLAttributes<HTMLDivElement>, 'onChange'> {
+  value?: string
+  onValueChange?: (value: string) => void
+}
+
+const RadioGroupContext = React.createContext<{
+  value?: string
+  onValueChange?: (value: string) => void
+}>({})
+
+const RadioGroup = React.forwardRef<HTMLDivElement, RadioGroupProps>(
+  ({ className, value, onValueChange, ...props }, ref) => {
+    return (
+      <RadioGroupContext.Provider value={{ value, onValueChange }}>
+        <div
+          ref={ref}
+          className={cn("grid gap-2", className)}
+          role="radiogroup"
+          {...props}
+        />
+      </RadioGroupContext.Provider>
+    )
+  }
+)
 RadioGroup.displayName = "RadioGroup"
 
 const RadioGroupItem = React.forwardRef<
   HTMLInputElement,
   React.InputHTMLAttributes<HTMLInputElement>
->(({ className, ...props }, ref) => {
+>(({ className, value, ...props }, ref) => {
+  const context = React.useContext(RadioGroupContext)
+  
   return (
     <input
       type="radio"
       ref={ref}
+      value={value}
+      checked={context.value === value}
+      onChange={(e) => {
+        if (e.target.checked && context.onValueChange) {
+          context.onValueChange(value as string)
+        }
+      }}
       className={cn(
         "h-4 w-4 rounded-full border border-primary text-primary focus:outline-none focus:ring-2 focus:ring-primary focus:ring-offset-2",
         className
